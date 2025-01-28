c5df93b66585f45025786dc82b77ef075e42fb8b
nivetha238 <srnive2004@gmail.com>
2025-01-28 22:45:49 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class age
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the age");
+int age = sc.nextInt();
+if(age>=0 && age<=12)
+{
+System.out.println("Child");}
+else if(age>=13 && age<=19){
+System.out.println("Teenager");}
+else if(age>=20 && age<=64){
+System.out.println("Adult");}
+else if(age>=65){
+System.out.println("Senior");}
+else
+{
+System.out.println("Invalid Age");
+}
+}
+}
