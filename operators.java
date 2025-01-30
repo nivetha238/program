8d590f253d96d1b8a917d07e663c8b71ab365b77
nivetha238 <srnive2004@gmail.com>
2025-01-30 22:25:23 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,14 @@
+public class operators
+{
+public static void main(String[]args)
+{
+int num1 = 15;
+int num2 = 20;
+System.out.println(num1+" < "+num2+" is "+ (num1<num2));
+System.out.println(num1+" <= "+num2+" is " + (num1<=num2));
+System.out.println(num1+" > "+num2+" is " + (num1>=num2));
+System.out.println(num1+" <= "+num2+" is " + (num1<=num2));
+System.out.println(num1+" == "+num2+" is "+ (num1==num2));
+System.out.println(num1+" != "+num2+" is " + (num1!=num2));
+}
+}
