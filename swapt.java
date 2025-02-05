01e07e67f8a89d0d41ebfb5f1f7e33ca98f46cd8
nivetha238 <srnive2004@gmail.com>
2025-02-05 21:25:12 +0530

    Add files via upload

---
+++ swapt.java
@@ -0,0 +1,12 @@
+public class swapt
+{
+public static void main(String[]args)
+{
+int a=5;
+int b=8;
+System.out.println("before swap: a="+a+"b="+b);
+int temp = a;
+a=b;
+b=temp;
+System.out.println("after swap: a="+a+"b="+b);
+}}
