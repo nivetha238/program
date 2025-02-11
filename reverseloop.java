d460706d68269d90b96df1bbe7d8288fef0afb50
nivetha238 <srnive2004@gmail.com>
2025-02-11 18:49:18 +0530

    Add files via upload

---
+++ reverseloop.java
@@ -0,0 +1,31 @@
+public class reverseloop
+{
+public static void main(String[] args)
+{
+String str = "Nivetha";
+String reversed = "";
+for(int i= str.length()-1;i>=0;i--){
+reversed += str.charAt(i);}
+System.out.println("Reversed string:"+reversed);
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
