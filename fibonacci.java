6c56f9c764dd8702797f94db7d5b076b75e2ebbd
nivetha238 <srnive2004@gmail.com>
2025-02-13 00:26:15 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+public class fibonacci
+{
+public static void main(String[] args)
+{
+int num=10,num1=0,num2=1,num3;
+for(int i=1;i<=num;i++){
+System.out.print(num1+",");
+num3 = num1+num2;
+num1 = num2;
+num2 = num3;}
+}}
+
